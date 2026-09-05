e97cd73baffbc3c93edbd45026ba706af3c58b0f
Aya Elsayed Mahmoud <ayae32450@gmail.com>
2026-09-06 02:12:57 +0300

    refactor: move forgot password screen to feature-based structure

--- lib/features/auth/presentation/screens/forgot_password_screen.dart
+++ lib/features/auth/presentation/screens/forgot_password_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 import 'otp_screen.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
